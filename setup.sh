cf05cfc34f41d766f5e0ca0d41dbd3ebd2a77e23
Yuji Kawamoto <yukawamoto@gmail.com>
2012-01-01 22:07:22 +0900

    fix which brew

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ mkdir -p $PWD/.vim/autoload
 ln -s $PWD/.vim/bundle/vim-pathogen/autoload/pathogen.vim $PWD/.vim/autoload/
 
 # symlink for brew bash completion
-which brew && (
+which brew > /dev/null && (
   BREW_PREFIX=`brew --prefix`
   ln -s $BREW_PREFIX/Library/Contributions/brew_bash_completion.sh \
         $BREW_PREFIX/etc/bash_completion.d/brew_bash_completion.sh
