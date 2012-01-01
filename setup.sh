dcf4e88ecdff6ea15601f41c7d2fb14afad89716
Yuji Kawamoto <yukawamoto@gmail.com>
2012-01-01 21:16:39 +0900

    add symlink for brew bash completion

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 git submodule init
 git submodule update
 
+# symlink for vim-pathogen
 mkdir -p $PWD/.vim/autoload
 ln -s $PWD/.vim/bundle/vim-pathogen/autoload/pathogen.vim $PWD/.vim/autoload/
+
+# symlink for brew bash completion
+ln -s `brew --prefix`/Library/Contributions/brew_bash_completion.sh \
+    `brew --prefix`/etc/bash_completion.d/brew_bash_completion.sh@
 
