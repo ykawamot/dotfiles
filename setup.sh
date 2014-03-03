470b3ae20a404c872fdd545f90fdc4f1173825e7
Yuji Kawamoto <yukawamoto@gmail.com>
2014-03-04 02:13:55 +0900

    make symlink move to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,46 @@
-#!/bin/sh -x
+#!/bin/bash
+
+test -t 1 && {
+  C_PREFIX='\033[1;31m'
+  C_SUFFIX='\033[0m'
+}
 
 # symlink for brew bash completion
-which brew > /dev/null && (
+which brew > /dev/null && {
   BREW_PREFIX=`brew --prefix`
-  brew install bash-completion
-  ln -s $BREW_PREFIX/Library/Contributions/brew_bash_completion.sh \
-        $BREW_PREFIX/etc/bash_completion.d/brew_bash_completion.sh
+  brew ls bash-completion > /dev/null && {
+    test ! -e $BREW_PREFIX/etc/bash_completion.d/brew_bash_completion.sh && {
+      echo "ln -s $BREW_PREFIX/Library/Contributions/brew_bash_completion.sh \\"
+      echo "      $BREW_PREFIX/etc/bash_completion.d/brew_bash_completion.sh"
+    }
+  }
+}
+
+DOT_FILES=(
+  .bash_completion
+  .bash_completion.d
+  .bash_profile
+  .bashrc
+  .bashrc.d
+  .digrc
+  .dir_colors
+  .gitconfig
+  .gitignore
+  .rspec
+  .vim
+  .vimrc
 )
+
+for file in ${DOT_FILES[@]} ; do
+    src=$PWD/$file
+    dst=$HOME/$file
+
+    if [ ! -e $src ] ; then
+        echo -e "# ${C_PREFIX}Warning${C_SUFFIX}: missing $src"
+    elif [ -e $dst ] ; then
+        echo -e "# ${C_PREFIX}Warning${C_SUFFIX}: exists $dst"
+    else
+        echo "ln -s $src $dst"
+    fi
+done
+
