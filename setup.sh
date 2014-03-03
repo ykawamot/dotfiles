10c1bd0f4cd86fca88968f1d24af0769ddee2260
Yuji Kawamoto <yukawamoto@gmail.com>
2014-03-04 00:17:50 +0900

    remove git submodule and vim-pathogen

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 #!/bin/sh -x
 
-git submodule init
-git submodule update
-
-# symlink for vim-pathogen
-mkdir -p $PWD/.vim/autoload
-ln -s $PWD/.vim/bundle/vim-pathogen/autoload/pathogen.vim $PWD/.vim/autoload/
-
 # symlink for brew bash completion
 which brew > /dev/null && (
   BREW_PREFIX=`brew --prefix`
