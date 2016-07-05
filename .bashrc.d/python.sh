2177d03858fcd4027ed1ded0cc90bda75699c193
Yuji Kawamoto <yukawamoto@gmail.com>
2016-07-06 00:44:00 +0900

    add quote

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -1,15 +1,21 @@
-# git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-test -d $HOME/.pyenv && {
-  export PYENV_ROOT=$HOME/.pyenv
-  pathprepend $PYENV_ROOT/bin
+#
+# .bashrc.d/python.sh
+#
+
+# pyenv
+# $ git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+test -d "$HOME/.pyenv" && {
+  export PYENV_ROOT="$HOME/.pyenv"
+  pathprepend "$PYENV_ROOT/bin"
 
   eval "$(pyenv init -)"
-  pathprepend $PYENV_ROOT/shims
+  pathprepend "$PYENV_ROOT/shims"
 
-  # git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-  test -d $PYENV_ROOT/plugins/pyenv-virtualenv && {
+  # pyenv-virtualenv
+  # $ git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+  test -d "$PYENV_ROOT/plugins/pyenv-virtualenv" && {
     eval "$(pyenv virtualenv-init -)"
-    pathprepend $PYENV_ROOT/plugins/pyenv-virtualenv/shims
+    pathprepend "$PYENV_ROOT/plugins/pyenv-virtualenv/shims"
   }
 }
 
