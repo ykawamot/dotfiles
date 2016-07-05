96fa230c806f4c83bd1b594c2341d3dadc20aafc
Yuji Kawamoto <yukawamoto@gmail.com>
2016-07-06 00:41:42 +0900

    add hub install command

--- .bashrc.d/git.sh
+++ .bashrc.d/git.sh
@@ -1,9 +1,15 @@
+#
+# .bashrc.d/git.sh
+#
+
 # __git_ps1()
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWUPSTREAM=1
 GIT_PS1_SHOWUNTRACKEDFILES=
 GIT_PS1_SHOWSTASHSTATE=1
 
+# hub
+# $ curl http://hub.github.com/standalone -sLo ~/bin/hub && chmod +x ~/bin/hub
 which hub &>/dev/null && {
   eval "$(hub alias -s)"
 }
