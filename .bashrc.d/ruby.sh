50812f63fa2409d09510cf9549be0b8a0f6dd1fa
ykawamot <yukawamoto@gmail.com>
2017-11-14 16:17:05 +0900

    add rbenv

--- .bashrc.d/ruby.sh
+++ .bashrc.d/ruby.sh
@@ -11,3 +11,9 @@ test -d "$HOME/.rvm" && {
   # Load RVM into a shell session *as a function*
   [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 }
+
+# rbenv
+[[ -d ~/.rbenv  ]] && {
+  pathappend "$HOME/.rbenv/bin"
+  eval "$(rbenv init -)"
+}
