f1ad11b29ea4e0a33ae23132c18cdf5bec63a985
ykawamot <ykawamot@yahoo-corp.jp>
2019-01-28 00:05:55 +0900

    fix for homebrew

--- .bashrc.d/homebrew.sh
+++ .bashrc.d/homebrew.sh
@@ -2,21 +2,35 @@
 # .bashrc.d/homebrew.sh
 #
 
+which brew &>/dev/null || return
+
+eval $(brew shellenv)
+
 # coreutils
 # $ brew install coreutils
-which brew &>/dev/null && {
-  GNU_PREFIX=$(brew --prefix coreutils 2>/dev/null)
-  test -d "$GNU_PREFIX" && {
-    pathprepend "$GNU_PREFIX/libexec/gnubin"
-    pathprepend "$GNU_PREFIX/libexec/gnuman" MANPATH
-  }
-  unset GNU_PREFIX
+GNU_PREFIX=$(brew --prefix coreutils 2>/dev/null)
+test -d "$GNU_PREFIX" && {
+  pathprepend "$GNU_PREFIX/libexec/gnubin"
+  pathprepend "$GNU_PREFIX/libexec/gnuman" MANPATH
 }
+unset GNU_PREFIX
 
 # suppress `brew doctor` warnings
-test -d $HOME/.pyenv/shims && {
+test -d "$HOME/.pyenv/shims" && {
   function brew() {
     local PATH=${PATH/$HOME\/.pyenv\/shims:/}
     command brew "$@"
   }
 }
+
+LIBS=(
+  apr
+  apr-util
+  curl
+  llvm
+  )
+
+for lib in "${LIBS[@]}" ; do
+  BINDIR=/usr/local/opt/${lib}/bin
+  [[ -d ${BINDIR} ]] && pathprepend ${BINDIR}
+done
